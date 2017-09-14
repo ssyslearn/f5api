ddf59d620753ccee4a3b6bf178034841d88609cd
root <root@host-192-168-1-93.proto.skp>
2017-09-14 18:02:49 +0900

    extract apicall_with_insertdb

--- VirtualServerList.py
+++ VirtualServerList.py
@@ -56,6 +56,26 @@ class VirtualServerList(Resource):
             cur.close()
             conn.close()
 
+    def apicall_with_insertdb(self, uri, cmd):
+        try:
+            r = requests.post(self.url+uri, auth=(self.username, self.password), \
+                        data = json.dumps(cmd), \
+                        headers=self.headers, verify=False)
+
+                #
+                # have to implement error handling of requests.post
+                # if apiError in json.loads(r.text).keys(), then error handling
+                #
+
+            with self.db_connect() as (conn, cur):
+                s = """ INSERT INTO API (method, api_uri, api_data, username, date) VALUES ('%s', '%s', '%s', '%s', now() ) """ % ('POST', uri, json.dumps(cmd), request.remote_user)
+                cur.execute(s)
+                conn.commit()
+            return jsonify(r.text)
+        except:
+            return 'cannot create virtual server'
+
+
     def get(self):
         try:
             r = requests.get(self.url+command.virtuals, auth=(self.username, self.password), headers=self.headers, params=self.payload, verify=False)
@@ -75,23 +95,7 @@ class VirtualServerList(Resource):
         cmd = json.loads(cmd.split("-d")[-1].split("'")[1])
 
         if Valid.valid_args(args, cmd):
-            try:
-                r = requests.post(self.url+uri, auth=(self.username, self.password), \
-                         data = json.dumps(cmd), \
-                         headers=self.headers, verify=False)
-
-                #
-                # have to implement error handling of requests.post
-                # if apiError in json.loads(r.text).keys(), then error handling
-                #
-
-                with self.db_connect() as (conn, cur):
-                    s = """ INSERT INTO API (method, api_uri, api_data, username, date) VALUES ('%s', '%s', '%s', '%s', now() ) """ % ('POST', uri, json.dumps(cmd), request.remote_user)
-                    cur.execute(s)
-                    conn.commit()
-                return jsonify(r.text)
-            except:
-                return 'cannot create virtual server'
+            return self.apicall_with_insertdb(uri, cmd)
         else:
             return 'You must request with virtual_server_name and destiantion and pool'
 
